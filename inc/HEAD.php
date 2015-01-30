9e7327e634a5d87161336228592c9304a2fa0ed5
AuTiMoThY <m62466@gmail.com>
2015-01-30 20:24:52 +0800

    V6.0.0 大整理 。 foundation 停用

--- inc/HEAD.php
+++ inc/HEAD.php
@@ -1,10 +1,11 @@
-<meta name="title" content="" />
-<meta name="keywords" content="">
+<!-- <meta name="title" content="" /> -->
+<meta name="keywords" content=""><!-- 關鍵字 -->
 <meta name="description" content="" /><!-- 網站描述 -->
 <meta name="Copyright" content="" /><!-- 版權宣告 -->
+<meta name="robots" content="index,follow">
 
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, user-scalable=yes">
 <!-- Don't forget to set your site up: http://google.com/webmasters -->
 <meta name="google-site-verification" content="" />
 
@@ -14,7 +15,6 @@
 <link rel="stylesheet" href="css/ripples.css">
 <link rel="stylesheet" href="css/material-wfont.css">
 <link rel="stylesheet" href="css/font-awesome.css">
-<link rel="stylesheet" href="css/foundation.css" />
 <!-- <link rel="stylesheet" href="asset/fancybox/jquery.fancybox.css" /> -->
 <link rel="stylesheet" href="asset/FlexSlider/flexslider.css" />
 <!-- <link rel="stylesheet" href="css/effect2.css" /> -->
