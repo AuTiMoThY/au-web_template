aca1d2a628a9159d4180df6c3588522897cf49f7
AuTiMoThY <m62466@gmail.com>
2015-05-29 10:08:48 +0800

    V8.0.3  Changes to be committed: 	deleted:    _Source/index.html 	new file:   dist/inc/headleader.php 	modified:   index.php 	new file:   plugin/animation.gsap.js

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<!doctype html>
-<!--[if lt IE 7 ]> <html class="ie ie6 ie-lt10 ie-lt9 ie-lt8 ie-lt7 no-js" lang="zh"> <![endif]-->
-<!--[if IE 7 ]>    <html class="ie ie7 ie-lt10 ie-lt9 ie-lt8 no-js" lang="zh"> <![endif]-->
-<!--[if IE 8 ]>    <html class="ie ie8 ie-lt10 ie-lt9 no-js" lang="zh"> <![endif]-->
-<!--[if IE 9 ]>    <html class="ie ie9 ie-lt10 no-js" lang="zh"> <![endif]-->
-<!--[if gt IE 9]><!--><html class="no-js" lang="zh"><!--<![endif]-->
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<?php
+  include_once 'dist/inc/headleader.php';
+ ?>
+
 <title></title>
 
 <?php
@@ -15,13 +10,13 @@
 // CSS
 // Script in the HEAD
 // -------------------------------
-  include_once 'inc/HEAD.php';
+  include_once 'dist/inc/head.php';
  ?>
 
 
 <?php
 //app
-  include_once 'inc/Social.php';
+  include_once 'dist/inc/social.php';
  ?>
 
 <!--[if lt IE 9]>
@@ -39,14 +34,14 @@ Au Template
 // -------------------------------
 // Script in the FOOT
 // -------------------------------
-  include_once 'inc/ScriptFOOT.php';
+  include_once 'dist/inc/scriptfoot.php';
  ?>
 
 <?php
 // -------------------------------
 // google analytics
 // -------------------------------
-  include_once 'inc/ga.php';
+  include_once 'dist/inc/ga.php';
  ?>
 
 </body>
