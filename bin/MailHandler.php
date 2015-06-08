6c16fc6a3c49e2e94f1c9e01f7d4e32a52053e00
AuTiMoThY <m62466@gmail.com>
2015-06-08 15:33:12 +0800

    - font-awesome 整理
    - fixes reset.css img
    - add fbscript.php
    - fixes MailHandler.php
    - add DF_ICON

--- bin/MailHandler.php
+++ bin/MailHandler.php
@@ -1,13 +1,15 @@
 <?
+    $project_name = "project_name";
+
 	if ( isset($_GET["workflag"]) && $_GET["workflag"] == "true" ) {
     // 寄送通知信         
-    $temp_mail_to = "m62466@gmail.com, goothlogo@gmail.com";
+    $temp_mail_to = "m62466@gmail.com";
     //$temp_mail_to = "mao@ecmd.com.tw";
      
-    $temp_mail_subject=" 谷思設計-聯絡我們新訊息 ";
+    $temp_mail_subject=$project_name." - 聯絡我們新訊息";
     $subject = "=?UTF-8?B?" . base64_encode($temp_mail_subject) . "?=";
      
-    $message = "<html><head><title>谷思設計 - 聯絡我們表單</title></head><meta http-equiv=Content-Type content='text/html; charset=utf-8'><body>";
+    $message = "<html><head><title>".$project_name." - 聯絡我們表單</title></head><meta http-equiv=Content-Type content='text/html; charset=utf-8'><body>";
 	$message .= "<table border=1 cellpadding=0 cellspacing=0 width=580 >";
 	// $message .= "<tr><th align='center' width=200>姓名</td><td> &nbsp; ".$_GET["name"]."</td>";	   
 	$message .= "<tr><th align='center' width=200>公司名稱</td><td> &nbsp; ".$_GET["name"]."</td>";
@@ -21,12 +23,12 @@
     // 用本地郵件伺服器處理
     $temp_mail_headers="MIME-Version:1.0\n";
     $temp_mail_headers.="Content-type:text/html;charset=UTF-8\n";
-    $temp_mail_headers.= "From: 谷思設計<goothlogo@gmail.com> \n"; 
+    $temp_mail_headers.= "From: ".$project_name."<goothlogo@gmail.com> \n"; 
 	$temp_mail_message=$temp_mail_content; 
     $result = mail($temp_mail_to,$subject,$message,$temp_mail_headers); 
 	
 	// 寄給開案人       
-    $temp_mail_subject=" 您在 谷思設計 http://goothlogo.com.tw/ 的留言 ";
+    $temp_mail_subject=" 您在 ".$project_name." http://goothlogo.com.tw/ 的留言 ";
     $subject = "=?UTF-8?B?" . base64_encode($temp_mail_subject) . "?=";
     $result = mail($_GET["email"],$subject,$message,$temp_mail_headers); 
    
--- dist/inc/Header.php
+++ dist/inc/Header.php
@@ -1,29 +1,7 @@
-<div id="fb-root"></div>
-<!-- <script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&appId=1436061923294298&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script> -->
-
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '494835137321739',
-      xfbml      : true,
-      version    : 'v2.2'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/zh_TW/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
+<?php
+// fb-root
+  include_once INC_PATH.'fbscript.php';
+ ?>
 
 <?php 
 require_once 'inc/aunav.php';
